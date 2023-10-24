7ed67ff4c5a3c32f7dee3cd671ae2e8ab1676e65
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2023-10-24 10:30:05 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+	@Test
+	public void reverseInPlaceEmptyTest(){
+
+		int input1 = {};
+		ArrayExamples.reverseInPlace(input1);
+		assertArrayEquals(new int[]{}, input1);
+	}
+
+	@Test
+	public void reversedBasicArrayTest()
+	{
+		int input1 = {1,2,3,4};
+		assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input1};
+		
+	}
 }
